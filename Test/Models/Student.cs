8946ad59557123c38949f675ed3417a7854d3b59
Abdus Salam <salamcseiu21@gmail.com>
2018-11-20 09:44:55 +0600

    Add Student Model

---
+++ Test/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
